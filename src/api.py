1ac6903ac3e42aa734053c4987b84dea3672c9aa
mueller <mueller@mbits.info>
2019-11-26 13:22:30 +0100

    Checkpoint commit as of 2019-11-26-13-22-30.

--- src/api.py
+++ src/api.py
@@ -1,8 +1,26 @@
 import abc
 import os
+import shutil
 from typing import List, Optional
-import tempfile
 from pdf2image import convert_from_path
+from PIL import Image
+from pytesseract import pytesseract
+import textract
+import hashlib
+from whoosh.index import create_in, open_dir
+from whoosh.fields import Schema, TEXT, ID
+import sys
+from whoosh.qparser import QueryParser
+from whoosh import scoring
+
+def sha256_sum(file_path):
+    h = hashlib.sha256()
+    b = bytearray(128 * 1024)
+    mv = memoryview(b)
+    with open(file_path, 'rb', buffering=0) as f:
+        for n in iter(lambda: f.readinto(mv), 0):
+            h.update(mv[:n])
+    return h.hexdigest()
 
 
 class FileProcessor:
@@ -11,31 +29,116 @@ class FileProcessor:
         pass
 
 
-class FilePathsFromDirectory:
-    def __init__(self):
-        self.directory = None  # type: Optional[str]
-        self.extensions = None  # type: List[Optional[str]]
-        self.file_processor = None  # type: FileProcessor
+class TextProcessor:
+    @abc.abstractmethod
+    def process(self, text, full_path, basename, file_name, ext):
+        pass
+
+
+class DirParser:
+    def __init__(self, directory, file_processor, extensions=[]):
+        self.directory = directory  # type: Optional[str]
+        self.file_processor = file_processor  # type: FileProcessor
+        self.extensions = extensions  # type: List[Optional[str]]
 
     def run(self):
-        for root, dirs, files in os.walk(self.directory.val):
+        for root, dirs, files in os.walk(self.directory):
             for basename in files:
                 file_name, ext = os.path.splitext(basename)
                 ext = ext[1:].lower()
-                if self.extensions is not None and ext not in self.extensions.items:
+                if len(self.extensions) > 0 and ext not in self.extensions:
                     continue
                 path = os.path.join(root, basename)
 
                 self.file_processor.process(path, basename, file_name, ext)
 
+
+class TextractProcessor(FileProcessor):
+    def __init__(self, text_processor):
+        self.text_processor = text_processor  # type: TextProcessor
+
+    def process(self, full_path, basename, file_name, ext):
+        try:
+            text = textract.process(full_path)
+            self.text_processor.process(text.decode('unicode_escape'), full_path, basename, file_name, ext)
+        except Exception as e:
+            print("error processing file {}: {}".format(full_path, str(e)))
+
+
+
 class TesseractProcessor(FileProcessor):
+    def __init__(self, text_processor):
+        self.text_processor = text_processor  # type: TextProcessor
 
     def process(self, full_path, basename, file_name, ext):
         if ext == "pdf":
-            with tempfile.TemporaryDirectory() as path:
-                images_from_path = convert_from_path(full_path, output_folder=path)
+            images = convert_from_path(full_path)
+        else:
+            images = [Image.open(full_path)]
+
+        text = ""
+        for image in images:
+            text = text + pytesseract.image_to_string(image)
+        print(text)
+        self.text_processor.process(text, full_path, basename, file_name, ext)
+
+
+class WhooshIndexer(TextProcessor):
+    def __init__(self, index_path, rebuild):
+        self.index_path = index_path
+        self.rebuild = rebuild
+        self.ix = None
+
+    def process(self, text, full_path, basename, file_name, ext):
+        sha256 = sha256_sum(full_path)
+        self.assert_index_writer()
+        print("indexing file {} with sha256 {}".format(full_path, sha256))
+        self.ix.add_document(title=basename, path=full_path, file_id=sha256, content=text, textdata=text)
+
+    def assert_index_writer(self):
+        if self.ix is not None:
+            return
+
+        if self.rebuild and os.path.exists(self.index_path):
+            print("deleting index at {}".format(self.index_path))
+            shutil.rmtree(self.index_path)
+
+        if os.path.exists(self.index_path):
+            self.ix = open_dir(self.index_path).writer()
+        else:
+            os.makedirs(self.index_path, 0o777, True)
+            schema = Schema(title=TEXT(stored=True), path=TEXT(stored=True), file_id=ID(stored=True), content=TEXT, textdata=TEXT(stored=True))
+            ix = create_in(self.index_path, schema)
+            self.ix = ix.writer()
+
+    def __del__(self):
+        if self.ix:
+            self.ix.commit()
 
 class DirOcr:
 
-    def run(self):
-        pass
+    def index(self, directory, index_path, rebuild, text_extract_library):
+        whoosh_indexer = WhooshIndexer(index_path, rebuild)
+
+        if text_extract_library == "tesseract":
+            file_processor = TesseractProcessor(whoosh_indexer)
+            extensions = ["png", "jpg", "jpeg", "pdf"]
+        else:
+            file_processor = TextractProcessor(whoosh_indexer)
+            extensions = []
+
+        dir_parser = DirParser(directory, file_processor, extensions)
+
+        dir_parser.run()
+
+    def search(self, index_path, query_str, num_docs):
+        ix = open_dir(index_path)
+
+        with ix.searcher(weighting=scoring.Frequency) as searcher:
+            query = QueryParser("content", ix.schema).parse(query_str)
+            results = searcher.search(query, limit=num_docs)
+            if len(results) > 0:
+                for i in range(0, len(results)):
+                    print(str(results[i].score), results[i]['title'], results[i]['path'])
+            else:
+                print("NO RESULTS!!!")
---
+++ src/dir_ocr_search.py
@@ -0,0 +1,16 @@
+import argparse
+import api
+
+# args
+parser = argparse.ArgumentParser(description='dir_ocr_search')
+parser.add_argument('index_path', type=str, help='the index path')
+parser.add_argument('--query_string', type=str, default=None, help='the query string')
+parser.add_argument('--num_docs', type=int, default=10, help='the maximum number of docs returned')
+args = parser.parse_args()
+
+dir_ocr = api.DirOcr()
+if args.query_string is None:
+    query_str = input("query_string: ")
+else:
+    query_str = args.query_string
+dir_ocr.search(args.index_path, query_str, args.num_docs)
